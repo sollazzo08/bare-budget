e84e865ca2d068cb6f3cd32ce687ea0be59a53ac
Michael Sollazzo <mike@MacBook-Pro.local>
2024-12-27 01:09:52 -0500

    Connected Prisma to DB and removed knex & objection + cleanup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,34 +2,16 @@ import express, {Request, Response} from'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
-import Knex from 'knex';
-import {Model} from 'objection';
 import api from './api';
 import {notFound,errorHandler} from './middlewares';
 import dotenv from 'dotenv';
 import { setUserFromTokenIfValid } from './api/auth/auth.middlewares';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
+const prisma = new PrismaClient();
 
 dotenv.config();
-
-const knex = Knex({
-  client: 'pg',
-  connection: {
-    database: process.env.POSTGRES_DB,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-  },
-  migrations: {
-    directory: './db/migrations',
-  },
-  seeds: {
-    directory: './db/seeds'
-  }
-});
-
-Model.knex(knex)
-
 
 app.use(morgan('tiny'));
 app.use(helmet());
